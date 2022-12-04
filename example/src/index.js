671bae4e46bec5342ec22e2b5ef22c8e1348ea98
Dilwoar Hussain <dilwoar.hussain@gmail.com>
2022-12-04 00:10:45 +0000

    Add colours component

--- example/src/index.js
+++ example/src/index.js
@@ -1,10 +1,9 @@
-import './index.css'
-
 import 'admin-lte'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App'
 import 'react-components/dist/index.css'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
---
+++ src/Colours/ColoursList.js
@@ -0,0 +1,23 @@
+const ColourList = {
+  blue: {
+    default: "#1C84C6",
+  },
+  red: {
+    default: "#ed5565",
+  },
+  green: {
+    default: "#19b394",
+    dark: "#15866f",
+    light: "#1ab394",
+  },
+  lime_green: {
+    default: "#5ab328",
+  },
+  grey: {
+    default: "#f4f6f9",
+    dark: "#d6d8db",
+    sidebar: "#2F4050",
+  },
+}
+
+export { ColourList }
